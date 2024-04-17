55e5d2e335e3426faba5d0edc7a08256c286f435
Yosi <lyt2006@naver.com>
2024-04-18 00:01:39 +0900

    .account html 추가

--- products/models.py
+++ products/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.conf import settings
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=50)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     image = models.ImageField(upload_to="image/", blank=True)
     
     author = models.ForeignKey(
-		settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
+		settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="product")
     
     like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="like_articles"
+        settings.AUTH_USER_MODEL, related_name="like_products"
     ) 
     
     def __str__(self):
@@ -22,7 +22,7 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="comments")
     
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Comment
+from .forms import ProductForm, CommentForm
+from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+     indexs= Product.objects.all().order_by('-id')
+     context = {
+         'indexs': indexs,
+         }
+     return render(request, 'products/index.html', context)
+
+
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid(): 
+            product=form.save(commit=False)
+            product.author = request.user
+            product.save()
+            return redirect('product_detail', product.pk)
+    else:
+        form = ProductForm()
+    
+    context= {'form':form}
+    return render(request, 'create.html', context)
