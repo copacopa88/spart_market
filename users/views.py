e7403d1bc6f0c26a9b6628bf30418028e88fbd95
Yosi <lyt2006@naver.com>
2024-04-19 23:13:58 +0900

    user 수정 3

--- users/views.py
+++ users/views.py
@@ -3,6 +3,9 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 
+def users(request):
+    return render(request, "users/users.html")
+
 
 def profile(request, username):
     member = get_object_or_404(get_user_model(), username=username)
